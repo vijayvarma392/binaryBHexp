1ef22e9f3320d54c44e8f15a9ffbf98afd8d5252
Vijay Varma <vvarma@caltech.edu>
2018-09-24 18:02:21 -0700

    Add generate_demos.sh

---
+++ generate_demos.sh
@@ -0,0 +1,5 @@
+python black_hole_scattering.py --q 2 --chiA 0.3 0.7 -0.1 --chiB 0.3 0.6 0.1 --omega_ref 1.8e-2 --save_file animations/precessing.gif
+#
+python black_hole_scattering.py --q 1.34 --chiA 0.62 -0.27 0.34 --chiB -0.62 0.27 0.34 --save_file animations/super_kick.gif
+#
+python black_hole_scattering.py --q 2 --chiA 0 0 -0.6 --chiB 0 0 0.8 --omega_ref 1.8e-2 --save_file animations/aligned.gif
