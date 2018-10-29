963e609a30a95e18262869fc7fd8457a133eb781
Vijay Varma <vvarma@caltech.edu>
2018-10-28 20:38:13 -0700

    Add __init__ and setup files

---
+++ binaryBHexp/__init__.py
@@ -0,0 +1,7 @@
+from . import binaryBHexp
+__author__ = binaryBHexp.__author__
+__email__ = binaryBHexp.__email__
+__copyright__ = binaryBHexp.__copyright__
+__license__ = binaryBHexp.__license__
+__version__ = binaryBHexp.__version__
+__doc__ = binaryBHexp.__doc__
---
+++ setup.py
@@ -0,0 +1,47 @@
+'''
+Standard setup.py to upload the code on pypi.
+Do:
+    python setup.py sdist bdist_wheel
+    twine upload dist/*
+'''
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Extract code version from __init__.py
+def get_version():
+    with open('binaryBHexp/binaryBHexp.py') as f:
+        for line in f.readlines():
+            if "__version__" in line:
+                return line.split('"')[1]
+
+setuptools.setup(
+    name="binaryBHexp",
+    version=get_version(),
+    author="Vijay Varma",
+    author_email="vvarma@caltech.edu",
+    description="Get the binary Black Hole experience, through visualizations.",
+    keywords='black-holes gravitational-waves visualization',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vijayvarma392/binaryBHexp",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'palettable',
+        'surfinBH',
+        'NRSur7dq2>=1.0.5',
+    ],
+    classifiers=[
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+)
