81f8e79b81c029e67860c49707dd07ddb1f81fa8
Vijay Varma <vvarma@caltech.edu>
2018-10-31 18:26:37 -0700

    Add stills to make_hangup_movie

--- make_hangup_movie.py
+++ make_hangup_movie.py
@@ -10,18 +10,19 @@ q = 1.
 chi_mag = 0.8
 
 cmdline_format = "./binaryBHexp --q {:.2f} " \
-    "--chiA 0 0 {:.2f} --chiB 0 0 {:.2f} --wave_time_series " \
+    "--chiA 0 0 {:.2f} --chiB 0 0 {:.2f} " \
+    "--wave_time_series --no_freeze_near_merger --stop_after_ringdown " \
     "--save_file animations/{}.mp4"
 
 # Generate individual movies
 cmdline = cmdline_format.format(q, chi_mag, chi_mag, "hangup_aligned")
-os.system(cmdline)
+#os.system(cmdline)
 
 cmdline = cmdline_format.format(q, 0, 0, "hangup_nonspin")
-os.system(cmdline)
+#os.system(cmdline)
 
 cmdline = cmdline_format.format(q, -chi_mag, -chi_mag, "hangup_antialigned")
-os.system(cmdline)
+#os.system(cmdline)
 
 # Combine into a single mp4
 join_cmdline = "ffmpeg -i animations/hangup_aligned.mp4 " \
@@ -29,4 +30,10 @@ join_cmdline = "ffmpeg -i animations/hangup_aligned.mp4 " \
     + "-i animations/hangup_antialigned.mp4 " \
     + "-filter_complex \"[0:v:0][1:v:0][2:v:0]hstack=inputs=3\" " \
     + "animations/hangup.mp4"
-os.system(join_cmdline)
+#os.system(join_cmdline)
+
+
+# save stills
+still_times = [1, 29.2, 33.63, 39.1]
+for time in still_times:
+    os.system("ffmpeg -i animations/hangup.mp4 -ss 00:00:%.3f -vframes 1 animations/hangup_t%d.jpg"%(time, time))
